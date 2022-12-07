a6a5aaab67dea508aa8a71bbdedd5ed83c905e08
SH Sohel <shsohel.tc@gmail.com>
2022-12-07 22:49:24 +0600

    first commi454ff4t

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middleware = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const middlewares = jsonServer.defaults();
 
-server.use(middleware);
-server.use(router);
-server.listen(port);
+server.use(middlewares);
+server.use('', router);
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running');
+});
